58af55f6398e5960ba5125e87545256a39563c79
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-11-30 20:46:22 -0300

    feat: Adicionando CORS

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from joblib import load
 
 model = load('./random_forest_model.joblib')
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
     features = data['features']
-    prediction = model.predict([data['features']])
+    prediction = model.predict([features])
     return jsonify({'prediction': int(prediction[0])})
 
 if __name__ == '__main__':
