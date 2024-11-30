258f84adc8022be8208abd467c366407e3d80856
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-11-30 15:30:56 -0300

    Frontend do Modelo

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from flask import Flask, request, jsonify
 from joblib import load
+from flask_cors import CORS
 
 model = load('./random_forest_model.joblib')
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
     features = data['features']
-    print("Received features:", features)
     prediction = model.predict([data['features']])
     return jsonify({'prediction': int(prediction[0])})
 
