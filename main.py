1e9fe950b8e38e6fcd924c03d8ea49e888f9bfa1
Gabriel Antonin Pascoali <gabrielantonin999@gmail.com>
2024-11-30 20:37:28 -0300

    feat: Removendo CORS da aplicação

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from flask import Flask, request, jsonify
 from joblib import load
-from flask_cors import CORS
 
 model = load('./random_forest_model.joblib')
 
 app = Flask(__name__)
-CORS(app, resources={r"/predict": {"origins": ["https://copd-model-frontend.vercel.app", "http://localhost:3000"]}})
 
 @app.route('/predict', methods=['POST'])
 def predict():
